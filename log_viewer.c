938ee0639f9b4f381748824901e8f4802201e9b5
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-10-16 16:10:21 -0500

    Users can close the program with Escape.

--- log_viewer.c
+++ log_viewer.c
@@ -1,9 +1,23 @@
 #include <gtk/gtk.h>
 
+// Event
+gboolean keyPressCallback(GtkWidget *widget, GdkEventKey *event, gpointer data) {
+  if (event->keyval == GDK_KEY_Escape) {
+    exit(EXIT_SUCCESS);
+    return TRUE;
+  }
+  return FALSE;
+}
+
 int main(int argc, char *argv[]) {
+
   gtk_init(&argc, &argv);
 
   GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  // Events
+  gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
+  g_signal_connect(G_OBJECT(window), "key_press_event", G_CALLBACK(keyPressCallback), NULL);
+  g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
   gtk_widget_show_all(window);
   gtk_main();
