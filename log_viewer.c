54ca76b59928e95cfa43164d72f2d2dc25b553a3
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-10-16 16:05:39 -0500

    Initial source commit.

---
+++ log_viewer.c
@@ -0,0 +1,10 @@
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[]) {
+  gtk_init(&argc, &argv);
+
+  GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+  gtk_widget_show_all(window);
+  gtk_main();
+}
